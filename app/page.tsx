0db8247fc44f98001032f29ef0ce567d31d5bd4e
Lukas Jaronis <jaronisluke13@gmail.com>
2024-11-03 17:17:15 -0800

    Added an about me paragraph

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1>Nedas's Portfolio</h1>
+      <p>Somethinga about me</p>
     </div>
   );
 }
